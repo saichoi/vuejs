573477fcbd828b73d1de39106c37701c4c2ec0ba
dhchoi <dhchoi@grooo.co.jp>
2025-01-14 16:07:03 +0900

    Slot

--- cmp-adv-01-starting-setup/src/components/BadgeList.vue
+++ cmp-adv-01-starting-setup/src/components/BadgeList.vue
@@ -1,15 +1,17 @@
 <template>
-  <section>
-    <h2>Available Badges</h2>
-    <ul>
-      <li>
-        <base-badge type="admin" caption="ADMIN"></base-badge>
-      </li>
-      <li>
-        <base-badge type="author" caption="AUTHOR"></base-badge>
-      </li>
-    </ul>
-  </section>
+  <base-card>
+    <section>
+      <h2>Available Badges</h2>
+      <ul>
+        <li>
+          <base-badge type="admin" caption="ADMIN"></base-badge>
+        </li>
+        <li>
+          <base-badge type="author" caption="AUTHOR"></base-badge>
+        </li>
+      </ul>
+    </section>
+  </base-card>
 </template>
 
 <style scoped>
