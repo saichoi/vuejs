91e71caa7f147d849a926915b5dbc73627b6d163
Choi Dahye <97278276+dahyechoigrooo@users.noreply.github.com>
2025-01-15 17:12:52 +0900

    Project2: Add Modal Dialog

---
+++ prj-cmp-01-starting-setup/src/UI/BaseDialog.vue
@@ -0,0 +1,89 @@
+<template>
+    <div>
+        <div class="overlay" @click="$emit('close')"></div>
+        <dialog open>
+            <header>
+                <slot name="header">
+                    <!-- 사용자가 HTML 을 지정하지 않았을 때 기본적으로 표시되는 부분 -->
+                    <h2>{{ title }}</h2>
+                </slot>
+            </header>
+            <section>
+                <slot></slot>
+            </section>
+            <menu>
+                <slot name="actions">
+                    <!-- 사용자가 HTML 을 지정하지 않았을 때 기본적으로 표시되는 부분 -->
+                    <base-button @click="$emit('close')">Close</base-button>
+                </slot>
+            </menu>
+        </dialog>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        'title': {
+            type: String,
+            required: false
+        }
+    },
+    emits: ['close']
+}
+</script>
+
+<style scoped>
+.overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.75);
+  z-index: 10;
+}
+
+dialog {
+  position: fixed;
+  top: 20vh;
+  left: 10%;
+  width: 80%;
+  z-index: 100;
+  border-radius: 12px;
+  border: none;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 0;
+  margin: 0;
+  overflow: hidden;
+}
+
+header {
+  background-color: #3a0061;
+  color: white;
+  width: 100%;
+  padding: 1rem;
+}
+
+header h2 {
+  margin: 0;
+}
+
+section {
+  padding: 1rem;
+}
+
+menu {
+  padding: 1rem;
+  display: flex;
+  justify-content: flex-end;
+  margin: 0;
+}
+
+@media (min-width: 768px) {
+  dialog {
+    left: calc(50% - 20rem);
+    width: 40rem;
+  }
+}
+</style>
--- prj-cmp-01-starting-setup/src/components/learning-resources/AddResource.vue
+++ prj-cmp-01-starting-setup/src/components/learning-resources/AddResource.vue
@@ -1,36 +1,60 @@
 <template>
-    <base-card>
-        <form @submit.prevent="submitData">
-            <div class="form-control">
-                <label for="title">Title</label>
-                <input type="text" id="title" name="title" ref="titleInput"/> 
-            </div>
-            <div class="form-control">
-                <label for="description">Description</label>
-                <textarea id="description" name="description" rows="3" ref="descInput"></textarea>
-            </div>
-            <div class="form-control">
-                <label for="link">Link</label>
-                <input id="link" name="link" type="url" ref="linkInput"/>
-            </div>
-            <div>
-                <base-button type="submit">Add Resource</base-button>
-            </div>
-        </form>
-    </base-card>
+    <div>
+        <base-dialog v-if="inputIsInvalid" title="Invalid Input" @close="confirmError">
+            <template #default>
+                <p>Unfortunately, at least one input value is invalid</p>
+                <p>Please Check all inputs and make sure you enter at least into each input field.</p>
+            </template>
+            <template #actions>
+                <base-button @click="confirmError">Okay</base-button>
+            </template>
+        </base-dialog>
+        <base-card>
+            <form @submit.prevent="submitData">
+                <div class="form-control">
+                    <label for="title">Title</label>
+                    <input type="text" id="title" name="title" ref="titleInput"/> 
+                </div>
+                <div class="form-control">
+                    <label for="description">Description</label>
+                    <textarea id="description" name="description" rows="3" ref="descInput"></textarea>
+                </div>
+                <div class="form-control">
+                    <label for="link">Link</label>
+                    <input id="link" name="link" type="url" ref="linkInput"/>
+                </div>
+                <div>
+                    <base-button type="submit">Add Resource</base-button>
+                </div>
+            </form>
+        </base-card>
+    </div>
 </template>
 
 <script>
 export default {
     inject: ['addResource'],
+    data() {
+      return {
+        inputIsInvalid: false
+      }
+    },
     methods: {
         submitData() {
             const enteredTitle = this.$refs.titleInput.value;
             const enteredDescription = this.$refs.descInput.value;
             const enteredUrl = this.$refs.linkInput.value;
+
+            if (enteredTitle.trim() === '' || enteredDescription.trim() === '' || enteredUrl.trim() === '') {
+                this.inputIsInvalid = true;
+                return;
+            }
             
             this.addResource(enteredTitle, enteredDescription, enteredUrl);
         },
+        confirmError() {
+            this.inputIsInvalid = false;
+        }
     }
 }
 </script>
