3bf753e42dc9d7bea62a6f11cddd75d328d97af2
Choi Dahye <97278276+dahyechoigrooo@users.noreply.github.com>
2025-01-15 13:49:53 +0900

    Project2: Setting and Init

---
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <ul></ul>
+</template>
+
+<script>
+export default {
+    storedResources: [
+        {
+            id: 'official-guide',
+            title: 'Official Guide',
+            description: "The official Vue.js document.",
+            link: "https//vuejs.org"
+        },
+        {
+            id: 'google',
+            title: 'Google',
+            description: "Learn to google...",
+            link: "https//google.com"
+        }
+    ]
+}
+</script>
