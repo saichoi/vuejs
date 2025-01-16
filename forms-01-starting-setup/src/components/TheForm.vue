f4f66db2132ac94acdf1b897cf396f60614dbaea
Choi Dahye <saichoi0424@gmail.com>
2025-01-16 09:29:05 +0900

    Forms: v-model and Dropdown

--- forms-01-starting-setup/src/components/TheForm.vue
+++ forms-01-starting-setup/src/components/TheForm.vue
@@ -13,7 +13,7 @@
     </div>
     <div class="form-control">
       <label for="referrer">How did you hear about us?</label>
-      <select id="referrer" name="referrer">
+      <select id="referrer" name="referrer" v-model="referrer">
         <option value="google">Google</option>
         <option value="wom">Word of mouth</option>
         <option value="newspaper">Newspaper</option>
@@ -60,17 +60,19 @@ export default {
   data() {
       return {
         userName: '',
-        userAge: null
+        userAge: null,
+        referrer: 'wom'
       }
     },
   methods: {
     submitForm() {
       console.log('Username', this.userName);
       this.userName = '';
-      console.log('User age:');
-      console.log(this.userAge);
+      console.log('User age:', this.userAge);
       console.log(31);
       this.userAge = null;
+      console.log('Referrer', this.referrer);
+      this.referrer = 'wom';
     }
   }
 }
