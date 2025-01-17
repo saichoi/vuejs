abec81d47fb8cabb596f252474e1671e8eab1a2e
Choi Dahye <saichoi0424@gmail.com>
2025-01-17 14:25:40 +0900

    Routing: Active Link

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -10,7 +10,8 @@ const router = createRouter({
     routes: [
         { path: '/teams', component: TeamsList},
         { path: '/users', component: UsersList}
-    ]
+    ],
+    linkActivClass: 'router-link-active'
 });
 
 const app = createApp(App)
