1228ef3254fef0475a842aaa5458efd87722e4c4
saichoi <dhchoi424@gmail.com>
2025-01-17 17:56:57 +0900

    Router: Nested Router

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -11,9 +11,11 @@ const router = createRouter({
     history: createWebHistory(), 
     routes: [
         { path: '/', redirect: '/teams'},
-        { path: '/teams', component: TeamsList }, // alias: '/' 옵션을 사용하면 도메인에서 /team와 동일한 화면을 보여준다.
+        { path: '/teams', component: TeamsList, children: [
+                { path: ':teamId', component: TeamMembers, props:true },// props: ture -> id를 전달하도록 유도한다.
+            ] 
+        }, // alias: '/' 옵션을 사용하면 도메인에서 /team와 동일한 화면을 보여준다.
         { path: '/users', component: UsersList},
-        { path: '/teams/:teamId', component: TeamMembers, props:true },// props: ture -> id를 전달하도록 유도한다.
         // { path: '/:notFoudn(.*)', redirect: '/teams'}
         { path: '/:notFoudn(.*)', component: NotFound } // 마지막에 작성해서 최종 수단으로 사용한다.
     ],
