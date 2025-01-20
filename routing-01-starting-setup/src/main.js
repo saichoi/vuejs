f3526a2725895043ca127cb4e9d066ba42771d49
Dahye Choi <saichoi0424@gmail.com>
2025-01-20 16:47:12 +0900

    Routing: Meta Data

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -16,6 +16,7 @@ const router = createRouter({
         { 
             name: 'teams', 
             path: '/teams', 
+            meta: { needsAuth: true },
             components: { default: TeamsList, footer: TeamsFotoer }, 
             children: [
                 { name: 'team-members', path: ':teamId', component: TeamMembers, props:true },// props: ture -> id를 전달하도록 유도한다.
@@ -54,6 +55,12 @@ const router = createRouter({
 router.beforeEach(function(to, from, next) {
     console.log('Global beforeEach');
     console.log(to, from);
+    if (to.meta.needsAuth) {
+        console.log('Needs auth!!');
+        next();
+    } else {
+        next();
+    }
     // if (to.name === 'team-members') {
     //     next();
     // } else {
