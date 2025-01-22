f7a7fd0f5e13624aa01101cf7a2a92a6afafa7a2
saichoi <dhchoi424@gmail.com>
2025-01-22 15:41:58 +0900

    Vuex: Passing Data to Mutation with Payload

--- vuex-01-starting-setup/src/main.js
+++ vuex-01-starting-setup/src/main.js
@@ -13,6 +13,9 @@ const store = createStore({
         // 각각의 컴포넌트에서 상태를 수정하는 로직을 넣는 게 아니라 이곳에서 전체적으로 로직을 넣고 어디서든 사용할 수 있다.
         increment(state) {
             state.counter = state.counter + 2;
+        },
+        increase(state, payload) {
+            state.counter = state.counter + payload.value;
         }
     }
 });
