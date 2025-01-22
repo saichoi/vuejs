edbd3c49158d5d0736930b68fad9e5e25074f659
saichoi <dhchoi424@gmail.com>
2025-01-22 17:15:39 +0900

    Vuex: Running Async Code with Actions

--- vuex-01-starting-setup/src/main.js
+++ vuex-01-starting-setup/src/main.js
@@ -12,10 +12,25 @@ const store = createStore({
     mutations: {
         // 각각의 컴포넌트에서 상태를 수정하는 로직을 넣는 게 아니라 이곳에서 전체적으로 로직을 넣고 어디서든 사용할 수 있다.
         increment(state) {
-            state.counter = state.counter + 2;
+            // 동기적으로 실행된다. 변경된 상황을 제때 받지 못하면 예상치 못한 에러가 발생할 수 있기 때문이다. => Action을 사용하여 대체할 수 있다.
+            // setTimeout(() => {
+                state.counter = state.counter + 2;
+            // }, 2000);
         },
         increase(state, payload) {
             state.counter = state.counter + payload.value;
+        }
+    },
+    // actions : 비동기식 코드를 허용한다.
+    actions: {
+        // mutations와 같은 이름을 사용한다.
+        increment(context) {
+            setTimeout(() => {
+                context.commit('increment');    
+            }, 2000);
+        },
+        increase(context, payload) {
+            context.commit('increase', payload);    
         }
     },
     getters: {
