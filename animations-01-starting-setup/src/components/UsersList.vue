92387eeeb0cc1bd6b1faf52c0019b5b53c1a8d44
Choi Dahye <saichoi0424@gmail.com>
2025-01-22 11:51:03 +0900

    Animations: Animating List with Transition Group

--- animations-01-starting-setup/src/components/UsersList.vue
+++ animations-01-starting-setup/src/components/UsersList.vue
@@ -1,8 +1,11 @@
 <template>
     <div>
-        <ul>
+        <!-- transition 컴포넌트와 달리 DOM에 랜더링 된다. 
+        tag 프로퍼티를 사용해서 랜더링 될 요소를 제어할 수 있다.
+        transition-group을 ul요소로 만들어준다. -->
+        <transition-group tag="ul" name="user-list"> 
             <li v-for="user in users" :key="user" @click="removeUser(user)">{{ user }}</li>
-        </ul>
+        </transition-group>
         <div>
             <input type="text" ref="userNameInput"/>
             <button @click="addUser">Add User</button>
@@ -39,5 +42,24 @@ li {
     border: 1px solid #ccc;
     padding: 1rem;
     text-align: center;
+}
+.user-list-enter-from {
+    opacity: 0;
+    transform: translateX(-30px);
+}
+.user-list-enter-active {
+    transition: all 1s ease-out;
+}
+.user-list-enter-to,
+.user-list-leave-from {
+    opacity: 1;
+    transform: translateX(0);
+}
+.user-list-leave-active {
+    transition: all 1s ease-in;
+}
+.user-list-leave-to {
+    opacity: 0;
+    transform: translateX(30px);
 }
 </style>
