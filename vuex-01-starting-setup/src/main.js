b82d691c2d6c5083dab81683bd58b7506f907e53
saichoi <dhchoi424@gmail.com>
2025-01-22 15:35:21 +0900

    Vuex: Passing Data to Mutations

--- vuex-01-starting-setup/src/main.js
+++ vuex-01-starting-setup/src/main.js
@@ -8,6 +8,12 @@ const store = createStore({
         return {
             counter: 0
         };
+    },
+    mutations: {
+        // 각각의 컴포넌트에서 상태를 수정하는 로직을 넣는 게 아니라 이곳에서 전체적으로 로직을 넣고 어디서든 사용할 수 있다.
+        increment(state) {
+            state.counter = state.counter + 2;
+        }
     }
 });
 
