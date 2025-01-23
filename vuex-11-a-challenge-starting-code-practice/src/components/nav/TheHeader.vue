19edc56deaa7c06aceb330b246e565e9b2343dfa
Choi Dahye <saichoi0424@gmail.com>
2025-01-23 17:43:27 +0900

    Vuex: Challenge2 Product and Cart(Local)

--- vuex-11-a-challenge-starting-code-practice/src/components/nav/TheHeader.vue
+++ vuex-11-a-challenge-starting-code-practice/src/components/nav/TheHeader.vue
@@ -10,7 +10,7 @@
         </li>
         <li>
           <router-link to="/cart">Cart</router-link>
-          <base-badge mode="elegant">{{ cart.qty }}</base-badge>
+          <base-badge mode="elegant">{{ cartQuantity }}</base-badge>
         </li>
         <li v-if="isLoggedIn">
           <router-link to="/admin">Admin</router-link>
@@ -28,6 +28,9 @@
 export default {
   inject: ['login', 'logout', 'cart'],
   computed: {
+    cartQuantity() {
+      return this.$store.getters['cart/quantity'];
+    },
     isLoggedIn() {
       return this.$store.getters.isAuthenticated;
     }
--- vuex-11-a-challenge-starting-code-practice/src/store/index.js
+++ vuex-11-a-challenge-starting-code-practice/src/store/index.js
@@ -1,6 +1,12 @@
 import { createStore } from 'vuex';
+import productsModule from './modules/products';
+import cartModule from './modules/cart';
 
 const store = createStore({
+    modules: {
+        prods: productsModule,
+        cart: cartModule
+    },
     state() {
         return {
             isLoggedIn: false
