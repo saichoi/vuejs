3c3b5375438fe38a0e0f10d36b4e540a0c37f975
saichoi <dhchoi424@gmail.com>
2025-01-23 12:18:14 +0900

    Vuex: Challenge Solution 3/3

--- vuex-11-a-challenge-starting-code/src/store/index.js
+++ vuex-11-a-challenge-starting-code/src/store/index.js
@@ -7,7 +7,33 @@ const store = createStore({
     modules: {
         prods: productsModule,
         cart: cartModule
-    }    
+    },
+    state() {
+        return {
+            isLoggendIn: false
+        }
+    },
+    mutations: {
+        login(state) {
+            state.isLoggendIn = true;
+        },
+        logout(state) {
+            state.isLoggendIn = false;
+        }
+    },
+    actions: {
+        login(context) {
+            context.commit('login');
+        },
+        logout(context) {
+            context.commit('logout');
+        }
+    },
+    getters: {
+        isAuthenticated(state) {
+            return state.isLoggendIn;
+        }
+    }
 });
 
 export default store;
