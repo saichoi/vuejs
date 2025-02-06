6cf39eae5d593524555fe6e15db6556902695a8a
Dahye Choi <saichoi0424@gmail.com>
2025-02-06 10:55:55 +0900

    Main Project: Adding Coaches to Vuex

--- main-prj-01-starting-setup/src/store/index.js
+++ main-prj-01-starting-setup/src/store/index.js
@@ -5,7 +5,17 @@ import coachesModule from './modules/coaches/index';
 const store = createStore({
     modules: {
         coaches: coachesModule
-    }    
+    },
+    state() {
+        return {
+            userId: 'c3'
+        }
+    },
+    getters: {
+        userId(state) {
+            return state.userId;
+        }
+    }
 });
 
 export default store;
--- main-prj-01-starting-setup/src/store/modules/coaches/getters.js
+++ main-prj-01-starting-setup/src/store/modules/coaches/getters.js
@@ -4,5 +4,10 @@ export default {
     },
     hasCoaches(state) {
         return state.coaches && state.coaches.length > 0;
+    },
+    isCoach(_, getters, _2, rooterGetters) {
+        const coaches = getters.coaches;
+        const userId = rooterGetters.userId; // 전역에 선언되어 있는 userId에 접근하기 위해서는 rootGetter을 사용해야한다.
+        return coaches.some(coach => coach.id === userId);
     }
 }
