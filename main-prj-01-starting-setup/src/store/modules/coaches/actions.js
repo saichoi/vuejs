53e6763e97f6e239d636817f80e1699f970926ee
Dahye Choi <saichoi0424@gmail.com>
2025-02-13 11:18:33 +0900

    Main Project: Fetching Coach Data GET HTTP Request

--- main-prj-01-starting-setup/src/store/modules/coaches/actions.js
+++ main-prj-01-starting-setup/src/store/modules/coaches/actions.js
@@ -25,5 +25,31 @@ export default {
             ...coachData,
             id: userId
         });
+    },
+    async loadCoaches(context) {
+        const response = await fetch(
+            `https://vue-http-demo-1181f-default-rtdb.firebaseio.com/coaches.json`
+        );
+        const responseData = await response.json();
+
+        if (!response.ok) {
+            // ...
+        }
+
+        const coaches = [];
+
+        for (const key in responseData) { // key = id
+            const coach = {
+                id: key,
+                firstName: responseData[key].firstName,
+                lastName: responseData[key].lastName,
+                description: responseData[key].description,
+                hourlyRate: responseData[key].hourlyRate,
+                areas: responseData[key].areas
+            };
+            coaches.push(coach);
+        }
+
+        context.commit('setCoaches', coaches);
     }
 }
