678f20bb6703adb7940576d02db6e09bbb66e4a5
saichoi <dhchoi424@gmail.com>
2025-02-13 12:13:45 +0900

    Main Project: Coaching HTTP Response Data

--- main-prj-01-starting-setup/src/pages/requests/RequestsReceived.vue
+++ main-prj-01-starting-setup/src/pages/requests/RequestsReceived.vue
@@ -1,10 +1,14 @@
 <template>
+  <base-dialog :show="!!error" title="An error occurred!" @close="handleError">
+    <p>{{ error }}</p>
+  </base-dialog>
   <section>
     <base-card>
       <header>
         <h2>Requests Received</h2>
       </header>
-      <ul v-if="hasRequests">
+      <base-spinner v-if="isLoadding"></base-spinner>
+      <ul v-else-if="hasRequests && !isLoadding">
         <request-item
           v-for="req in receivedRequests"
           :key="req.id"
@@ -24,12 +28,35 @@ export default {
   components: {
     RequestItem
   },
+  data() {
+    return {
+      isLoadding: false,
+      error: null
+    }
+  },
   computed: {
     receivedRequests() {
       return this.$store.getters['requests/requests'];
     },
     hasRequests() {
       return this.$store.getters['requests/hasRequests'];
+    }
+  },
+  created() {
+    this.loadRequests();
+  },
+  methods: {
+    async loadRequests() {
+      this.isLoadding = true;
+      try {
+        await this.$store.dispatch('requests/fetchRequests');
+      } catch(error) {
+        this.error = error.message || 'Something failed!';
+      }
+      this.isLoadding = false;
+    },
+    handleError() {
+      this.error = null;
     }
   }
 };
--- main-prj-01-starting-setup/src/store/modules/requests/actions.js
+++ main-prj-01-starting-setup/src/store/modules/requests/actions.js
@@ -1,11 +1,49 @@
 export default {
-    contactCoach(context, paylaod) {
+    async contactCoach(context, paylaod) {
         const newRequest = {
-            id: new Date().toISOString(),
-            coachId: paylaod.coachId,
             userEmail: paylaod.email,
             message: paylaod.message
         }
+
+        const response = await fetch(`https://vue-http-demo-1181f-default-rtdb.firebaseio.com/requests/${paylaod.coachId}.json`, {
+            method: 'POST',
+            body: JSON.stringify(newRequest)
+        });
+
+        const responseData =  await response.json();
+
+        if (!response.ok) {
+            const error = new Error(responseData.message || 'Failed to send request.')
+            throw error;
+        }
+
+        newRequest.id = responseData.name;
+        newRequest.coachId = paylaod.coachId; // 서버에는 전송되지 않고 로컬데이터로 저장
+
         context.commit('addRequest', newRequest);
+    },
+    async fetchRequests(context) {
+        const coachId = context.rootGetters.userId; // store/index.js에 있는 userId를 가져온다.
+        const response = await fetch(`https://vue-http-demo-1181f-default-rtdb.firebaseio.com/requests/${coachId}.json`);
+        const responseData = await response.json();
+
+        if (!response.ok) {
+            const error = new Error(responseData.message || 'Failed to fetch requests.')
+            throw error;
+        }
+
+        const requests = [];
+
+        for (const key in responseData) {
+            const request = {
+                id: key,
+                coachId: coachId,
+                userEmail: responseData[key].userEmail,
+                message: responseData[key].message
+            };
+            requests.push(request);
+        }
+
+        context.commit('setRequests', requests);
     }
 }
