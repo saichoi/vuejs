0887adf2664ebe6169d59cb76b139979356dca6e
Dahye Choi <saichoi0424@gmail.com>
2025-02-17 11:28:39 +0900

    Auth: Adding An Authentication Page

--- main-prj-01-starting-setup/src/components/ui/BaseButton.vue
+++ main-prj-01-starting-setup/src/components/ui/BaseButton.vue
@@ -1,12 +1,10 @@
 <template>
-    <div>
-        <button v-if="!link" :class="mode">
-            <slot></slot>
-        </button>
-        <router-link v-else :to="to" :class="mode">
-            <slot></slot>
-        </router-link>
-    </div>
+  <button v-if="!link" :class="mode">
+    <slot></slot>
+  </button>
+  <router-link v-else :to="to" :class="mode">
+    <slot></slot>
+  </router-link>
 </template>
 
 <script>
@@ -25,7 +23,7 @@ export default {
         to: {
             type: String,
             required: false,
-            default: null
+            default: '/'
         }
     }
 }
