aa8df2bb1d4ddd257d2868cc94e4bb3e70c54d81
Dahye Choi <saichoi0424@gmail.com>
2025-03-19 17:18:23 +0900

    Auth: Signup and Login Bugfix

--- main-prj-01-starting-setup/src/store/modules/auth/actions.js
+++ main-prj-01-starting-setup/src/store/modules/auth/actions.js
@@ -1,6 +1,6 @@
 export default {
     async login(context, payload) {
-        const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=AIzaSyCB9ckNijEWrfatzcjYktDF_5YynrU08lA', {
+        const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCB9ckNijEWrfatzcjYktDF_5YynrU08lA', {
             method: 'POST',
             body: JSON.stringify({
                 email: payload.email,
@@ -25,7 +25,7 @@ export default {
         });
     },
     async signup(context, payload) {
-        const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=AIzaSyCB9ckNijEWrfatzcjYktDF_5YynrU08lA', {
+        const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCB9ckNijEWrfatzcjYktDF_5YynrU08lA', {
             method: 'POST',
             body: JSON.stringify({
                 email: payload.email,
