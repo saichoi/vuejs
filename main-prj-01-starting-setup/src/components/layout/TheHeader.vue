003dc15e9a98948974f99195a1494f493315b48a
saichoi <dhchoi424@gmail.com>
2025-03-19 17:25:39 +0900

    Auth: Adding a Logout Action

--- main-prj-01-starting-setup/src/components/layout/TheHeader.vue
+++ main-prj-01-starting-setup/src/components/layout/TheHeader.vue
@@ -6,6 +6,7 @@
                 <li><router-link to="/coaches">All Coaches</router-link></li>
                 <li v-if="isLoggedIn"><router-link to="/requests">Requests</router-link></li>
                 <li v-else><router-link to="/auth">Login</router-link></li>
+                <li v-if="isLoggedIn"><base-button @click="logout">Logout</base-button></li>
             </ul>
         </nav>
     </header>
@@ -16,6 +17,11 @@ export default {
   computed: {
     isLoggedIn() {
       return this.$store.getters.isAuthenticated;
+    }
+  },
+  methods: {
+    logout() {
+      this.$store.dispatch('logout');
     }
   }
 }
