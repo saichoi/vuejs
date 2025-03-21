66bfbbd528f8fa954bb75570d6a3500c34ad03e0
Dahye Choi <saichoi0424@gmail.com>
2025-03-21 17:18:11 +0900

    Deploy: Async Components

--- main-prj-01-starting-setup/src/router.js
+++ main-prj-01-starting-setup/src/router.js
@@ -1,13 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import CoachDetail from './pages/coaches/CoachDetail.vue';
+// import CoachDetail from './pages/coaches/CoachDetail.vue';
 import CoachesList from './pages/coaches/CoachesList.vue';
-import CoachRegistation from './pages/coaches/CoachRegistration.vue';
-import ContactCoach from './pages/requests/ContactCoach.vue';
-import RequestsReceived from './pages/requests/RequestsReceived.vue';
+// import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+// import ContactCoach from './pages/requests/ContactCoach.vue';
+// import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import NotFound from './pages/NotFound.vue';
-import UserAuth from './pages/auth/UserAuth.vue';
+// import UserAuth from './pages/auth/UserAuth.vue';
 import store from './store/index';
+
+const CoachDetail = () => import('./pages/coaches/CoachDetail.vue')
+const CoachRegistration = () => import('./pages/coaches/CoachRegistration.vue')
+const ContactCoach = () => import('./pages/requests/ContactCoach.vue')
+const RequestsReceived = () => import('./pages/requests/RequestsReceived.vue')
+const UserAuth = () => import('./pages/auth/UserAuth.vue')
 
 const router = createRouter({
   history: createWebHistory(),
@@ -22,7 +28,7 @@ const router = createRouter({
         { path: 'contact', component: ContactCoach }, // coaches/c1/contact
       ],
     },
-    { path: '/register', component: CoachRegistation, meta: { requiresAuth: true } }, // 로그인하지 않으면 접속할 수 없는 페이지
+    { path: '/register', component: CoachRegistration, meta: { requiresAuth: true } }, // 로그인하지 않으면 접속할 수 없는 페이지
     { path: '/requests', component: RequestsReceived, meta: { requiresAuth: true } }, // 로그인하지 않으면 접속할 수 없는 페이지
     { path: '/auth', component: UserAuth, meta: { requiresUnauth: true } }, // 로그인하면 더이상 접속할 수 없는 페이지
     { path: '/:notFound(.*)', component: NotFound },
