9a0cd451709e7114fec17d372bc587526c01c8fd
Dahye Choi <saichoi0424@gmail.com>
2025-03-24 18:01:59 +0900

    Composition: Working with Provide/Inject

--- composition-01-starting-setup/src/components/UserData.vue
+++ composition-01-starting-setup/src/components/UserData.vue
@@ -6,21 +6,23 @@
 </template>
 
 <script>
-import { computed } from 'vue';
+import { computed, inject } from 'vue';
 
 export default {
-    props: ['firstName', 'lastName', 'age'],
+    props: ['firstName', 'lastName'],
     setup(props, context) {
         const userName = computed(function() {
             // setup 을 호출하는 타이밍이 props를 호출하는 타이밍보다 빠르기 때문에 props 값을 정의하지 못하고 있다.
             return props.firstName + ' ' + props.lastName;
         });
 
+        const age = inject('userAge');
+
         console.log(context);
 
         // context.emit('save-data', 1) // this.$emit('save-date', 1);
 
-        return { userName }
+        return { userName, age }
     }
     // computed: {
     //     userName() {
