3169a6e909158bfd04a1d8d22b260699d4670fd4
saichoi <dhchoi424@gmail.com>
2025-03-24 18:10:33 +0900

    Composition: Lifecycle Hooks

--- composition-01-starting-setup/src/components/UserData.vue
+++ composition-01-starting-setup/src/components/UserData.vue
@@ -6,10 +6,24 @@
 </template>
 
 <script>
-import { computed, inject } from 'vue';
+import { 
+    computed, 
+    inject, 
+    onBeforeMount, 
+    onMounted, 
+    onBeforeUpdate,
+    onUpdated,
+    onBeforeUnmount,
+    onUnmounted
+  } from 'vue';
 
 export default {
     props: ['firstName', 'lastName'],
+    // Lifecycle
+    // beforeCreated, created가 실행되는 시점에 실행되기 때문에 라이프 사이클이 따로 존재하지 않는다. 
+    // beforeMount, mounted => onBeforeMount, onMounted
+    // beforeUpdate, updated => onBeforeUpdate, onUpdate
+    // beforeUnmount, unmounted => onBeforeMount, onMounted
     setup(props, context) {
         const userName = computed(function() {
             // setup 을 호출하는 타이밍이 props를 호출하는 타이밍보다 빠르기 때문에 props 값을 정의하지 못하고 있다.
@@ -19,6 +33,36 @@ export default {
         const age = inject('userAge');
 
         console.log(context);
+
+        onBeforeMount(function() {
+            console.log('onBeforeMount');
+        });
+
+
+        onMounted(function() {
+            console.log('onMounted');
+        });
+
+
+        onBeforeUpdate(function() {
+            console.log('onBeforeUpdate');
+        });
+
+
+        onUpdated(function() {
+            console.log('onUpdated');
+        });
+
+
+        onBeforeUnmount(function() {
+            console.log('onBeforeUnmount');
+        });
+
+
+        onUnmounted(function() {
+            console.log('onUnmounted');
+        });
+
 
         // context.emit('save-data', 1) // this.$emit('save-date', 1);
 
