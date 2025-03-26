d1515957c8be690c37949f6acc7bc6f902c2b9c7
Dahye Choi <saichoi0424@gmail.com>
2025-03-26 16:13:00 +0900

    Reuse: Custom Hooks Composables

--- reuse-04-composition-api-starting-project/src/components/AddUser.vue
+++ reuse-04-composition-api-starting-project/src/components/AddUser.vue
@@ -9,28 +9,21 @@
 </template>
 
 <script>
-import { ref } from 'vue';
 import UserAlert from './UserAlert.vue';
+import useAlert from '../hook/alert';
 
 export default {
   components: {
     UserAlert,
   },
   setup() {
-    const alertIsVisible = ref(false);
+    const [alertIsVisible, showAlert, hideAlert] = useAlert();
 
-    function showAlert() {
-      alertIsVisible.value = true;
-    }
-    function hideAlert() {
-      alertIsVisible.value = false;
-    }
-    
     return {
-      alertIsVisible,
+      alertIsVisible, 
       showAlert,
       hideAlert
-    };
+    }
   },
 };
 </script>
