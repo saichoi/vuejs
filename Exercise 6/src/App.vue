c5692b9fe9e80e31e300ab12aa561ed4beaf1078
Dahye Choi <saichoi0424@gmail.com>
2025-03-27 15:00:18 +0900

    Exercise 6

---
+++ Exercise 6/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <Header />
+        <hr>
+        <div class="row">
+            <ServerDetail />
+            <Servers />
+        </div>
+        <Footer />
+        <hr>
+  
+    </div>
+</template>
+
+<script>
+import Header from './Header.vue';
+import Footer from './Footer.vue';
+import ServerDetail from './ServerDetail.vue';
+import Servers from './Servers.vue';
+
+export default {
+    components: {
+        Header,
+        Footer,
+        ServerDetail,
+        Servers
+    }
+}
+</script>
+
+<style>
+
+</style>
