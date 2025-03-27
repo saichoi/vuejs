cd70b871709093ac34e74b56bb28b5e8d4a957c3
saichoi <dhchoi424@gmail.com>
2025-03-27 15:55:45 +0900

    Exercise 7

---
+++ Exercise 6/src/components/layout/server/Server.vue
@@ -0,0 +1,40 @@
+<template>
+    <ul class="list-group">
+        <li 
+            class="list-group-item"
+            v-for="server in servers" 
+            :key="server.id" 
+            @click="serverSelected(server)"
+            :class="{'selected': isSelected(server)}"
+        >
+            Server #{{ server.id }}
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: ['servers'],
+    data() {
+        return {
+            selectedId: null
+        }
+    },
+    methods: {
+        serverSelected(server) {
+            this.selectedId = server.id
+            this.$emit('server-selected', server);
+        },
+        isSelected(server) {
+            return server.id === this.selectedId;
+        }
+    }
+}
+</script>
+
+<style scoped>
+.selected {
+  background-color: #007bff; 
+  color: white;
+}
+</style>
---
+++ Exercise 6/src/components/layout/server/Servers.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <Server :servers="servers" @server-selected="handleSeverSelected"/>
+    </div>
+</template>
+
+<script>
+import Server from './Server.vue';
+
+export default {
+    components: {
+        Server
+    },
+    data() {
+        return {
+            servers: [
+                { id: 1, status: 'Normal'},
+                { id: 2, status: 'Critical'},
+                { id: 3, status: 'Unknown'},
+                { id: 4, status: 'Normal'}
+            ]
+        }
+    },
+    methods: {
+        handleSeverSelected(server) {
+            this.$emit('server-selected', server);
+        }
+    }
+}
+</script>
